6d88ef4b848807199fb147489bc18cd17ab6fed1
Anita <sharma.anita00001@gmail.com>
2023-02-14 20:26:00 +0545

    Add addBook, removeBook, updateStorage functions

--- book.js
+++ book.js
@@ -3,11 +3,32 @@ class Book {
   constructor() {
     this.booksList = this.getBookFromStorage() || [];
   }
+
   // add books
+  addBook(author, title){
+    let updatedBook = [
+      ...this.booksList,
+      {author, title}
+    ];
+    this.updateStorage(updatedBook);
+  }
 
   // remove books
+  removeBook(title){
+    let updatedBook = this.booksList.filter((it) => it.title !== title);
+    this.updateStorage(updatedBook);
+  }
+
+  // Get books
+  getBook(){
+    return this.booksList;
+  }
 
   // update storage
+  updateStorage(data){
+    localStorage.setItem('storage-book', JSON.stringify(data));
+    this.booksList = data;
+  }
 
   // get book from storage
   getBookFromStorage() {
