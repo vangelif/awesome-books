380be7cab91b4004fd607d06fcf23e5e696ee10d
Anita <sharma.anita00001@gmail.com>
2023-02-14 13:55:47 +0545

    Add Heading and Form

--- script.js
+++ script.js
@@ -1,18 +1,7 @@
 const targetBody = document.querySelector('body');
 
 const createContainer = document.createElement('section');
-createContainer.innerHTML = `<h1>Awesome Books</h1>`;
 targetBody.appendChild(createContainer);
-
-////////// Book Form ///////// 
-const createForm = document.createElement('form');
-createForm.id = 'form'
-createForm.innerHTML = `
-<input id="book-title" type="text" placeholder="Title"><br><br>
-<input id="author" type="name" placeholder="Author"><br><br>
-<button type="submit">Add</button>`;
-
-createContainer.appendChild(createForm);
 
 // Adding a Book // 
 const addBook = (title, author) => {
