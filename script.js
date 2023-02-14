b4ee336ebdf8fca821ddcbe98c1257beeefe256d
Vangelis <ev.fotiades@yahoo.com>
2023-02-14 12:06:18 +0200

    Add local storage functions

--- script.js
+++ script.js
@@ -6,26 +6,18 @@ const author = document.getElementById("author");
 const form = document.getElementById("form");
 const error = document.getElementById("error");
 
-// Adding a Book //
-const addBook = (title, author) => {
-  const book = { title, author };
-  const checkBooks = bookListArray.find((book) => book.title === title);
-  if (checkBooks) {
-    error.innerText = "Book with title exists!!";
-  } else {
-    error.innerHTML = "";
-    bookListArray.push(book);
-    displayBooks();
-  }
-};
+const updateLocalStorage = (data) => {
+  localStorage.setItem('storage-book', JSON.stringify(data));  
+} 
 
-// person 2
-const displayBookList = document.getElementById("table");
+const getLocalStorage = () => {
+  return JSON.parse(localStorage.getItem('storage-book'));
+}
 
 // Display Book List with author and remove button // person 2
 const displayBooks = () => {
   displayBookList.innerHTML = ``;
-  bookListArray.forEach((book) => {
+  bookListArray?.forEach((book) => {
     const bookItem = document.createElement("tbody");
     bookItem.innerHTML = `
       <div>
@@ -37,11 +29,36 @@ const displayBooks = () => {
     displayBookList.appendChild(bookItem);
   });
 };
+
+const initialize = () => {  
+  bookListArray = getLocalStorage() || [];
+  // console.log(bookListArray)
+  displayBooks();
+}
+
+// Adding a Book //
+const addBook = (title, author) => {
+  const book = { title, author };
+  const checkBooks = bookListArray.find((book) => book.title === title);
+  const checkAuthor = bookListArray.find((book) => book.author === author);
+  if (checkBooks && checkAuthor) {
+    error.innerText = "This book already exists!!";
+  } else {
+    error.innerHTML = "";
+    bookListArray.push(book);
+    updateLocalStorage(bookListArray);
+    displayBooks();
+  }
+};
+
+const displayBookList = document.getElementById("table");
+
 error.innerHTML = "";
-displayBooks();
+initialize();
 
 const deleteBook = (title) => {
   bookListArray = bookListArray.filter((it) => it.title !== title);
+  updateLocalStorage(bookListArray);
   displayBooks();
 };
 
@@ -57,7 +74,7 @@ document.addEventListener("click", (e) => {
 submitButton.addEventListener("click", (e) => {
   e.preventDefault();
   if (bookTitle.value.length === 0 || author.value.length === 0) {
-    error.innerText = "Fileds cannot be empty!";
+    error.innerText = "Fields cannot be empty!";
   } else {
     addBook(bookTitle.value, author.value);
     form.reset();
